75b1b5fdeb536b4d8c8d94736ee893e5e96962c4
ojuolatimi09 <ojuolatimi09@gmail.com>
2022-11-20 16:01:15 -0800

    errorfixes

--- index2.js
+++ index2.js
@@ -4,23 +4,28 @@ let sum = firsCard + secondCard
 let hasblackjack = false
 let isAlive = true
 let  message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-function startGame() {
-  if (false) {
+
+function startgame() {
+  cardsEl.textContent = "Cards: " + firsCard + "" + secondCard
+  sumEl.textContent = "Sum: " + sum 
+  if (sum<= 20) {
     message = "Do you want to draw a new card"
   }
-  else if(true) {
-    message = "Wohoo!, You've got Blackjack"
+  else if(sum === 21) {
+    message = "You've got Blackjack"
     hasblackjack = true
   } 
   else if (sum > 21) {
     message = "You're out of the game"
     isAlive = false
   }
-  console.log(message)
-  }
+  messageEl.textContent = message
+}
 
-
-
-
-
+function newcard() {
+  
+}
