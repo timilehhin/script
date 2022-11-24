51a984673c88ca8677f39ac30ef2c05311106570
ojuolatimi09 <ojuolatimi09@gmail.com>
2022-11-23 16:18:05 -0800

    errorfixes

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+//Arrays - ordered list of items
 
+let featuredPosts = [
+"Check out my netflix clone",  
+"Here's the code for my project", 
+"I've just relaunched my portfolio"
+]
+let experience = [
+    "timilehinojuola.com.ng"
+]
+
+let experiences = [
+    "Frontend developer at Xeneta",
+    "People counter for Norstat",
+    "CEO at Scrimba"
+]
+
+console.log (experiences.length)
+//console.log (experiences[1])
+//console.log (experiences[2])
+
+//let education = [
+   // "Word of faith group of schools, Abuja",
+    //"King's Care Universal School",
+    //"Deo-gratias"
+//]
+
+//let liences = [
+   // "No liences"
+//]
+
+//let skills = [
+    //"Patience",
+    //"Love for the work"
+//]
